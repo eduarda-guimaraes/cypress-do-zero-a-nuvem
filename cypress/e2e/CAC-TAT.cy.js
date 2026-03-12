0299512e2cb6ea858229e0246b3b31cc7f8122e3
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2026-03-11 21:52:36 -0300

    add beforeEach with cy.visit and two title assertion tests

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Central de Atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
+      cy.visit('http://127.0.0.1:5500/src/index.html')
   })
-})
+
+  it('verifica o título da aplicação', () => {
+    cy.title().should('eq', 'Central de Atendimento ao Cliente TAT') // eq = be.equal
+  })
+  
+  it('testa o título', () => {
+      cy.get('h1[id="title"]').as('titleName').should('be.visible')
+      cy.get('@titleName').should('have.text', 'CAC TAT')
+  })
+
+} )
