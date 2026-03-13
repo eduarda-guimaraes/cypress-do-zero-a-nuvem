a57b95a5d5c0be2108d85da80497205d4b697f89
Eduarda Guimarães <eduardaguima2007@yahoo.com>
2026-03-12 21:26:08 -0300

    exercise one: move visit to beforeEach and add form fill & submit test

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -8,8 +8,36 @@ describe('Central de Atendimento ao Cliente TAT', () => {
   })
   
   it('testa o título', () => {
-      cy.get('h1[id="title"]').as('titleName').should('be.visible')
+      cy.get('h1[id="title"]').as('titleName')
+      .should('be.visible')
       cy.get('@titleName').should('have.text', 'CAC TAT')
+  })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    // name and lastname
+    cy.get('input[id="firstName"]').as('firstName') // input[id="firstName"] = #firstName
+    .type('Eduarda')
+    cy.get('@firstName').should('have.value', 'Eduarda')
+    .should('be.visible')
+    cy.get('input[id="lastName"]').as('lastName') // input[id="lastName"] = #lastName
+    .type('Guimarães')
+    cy.get('@lastName').should('have.value', 'Guimarães')
+    .should('be.visible')
+
+    // email
+    cy.get('input[id="email"]').as('email').type('aeduardaguimaraes@gmail.com') // input[id="email"] = #email
+    cy.get('@email').should('have.value', 'aeduardaguimaraes@gmail.com').should('be.visible')
+
+    // how can we help you?
+    cy.get('textarea[id="open-text-area"]').as('feedback') // textarea[id="open-text-area"] = #open-text-area
+    .type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien eget nunc efficitur varius. Sed at ligula a enim efficitur efficitur. Donec ac odio id nisl convallis tincidunt. In hac habitasse platea dictumst. Curabitur ut felis sed enim efficitur efficitur. Donec ac odio id nisl convallis tincidunt. In hac habitasse platea dictumst. Curabitur ut felis sed enim efficitur efficitur.')
+    cy.get('@feedback').should('have.value', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel sapien eget nunc efficitur varius. Sed at ligula a enim efficitur efficitur. Donec ac odio id nisl convallis tincidunt. In hac habitasse platea dictumst. Curabitur ut felis sed enim efficitur efficitur. Donec ac odio id nisl convallis tincidunt. In hac habitasse platea dictumst. Curabitur ut felis sed enim efficitur efficitur.')
+    .should('be.visible')
+
+    // button submit
+    cy.get('button[type="submit"]').as('submitButton').should('be.visible').click()
+    cy.get('span[class="success"]').as('successMessage').should('be.visible')
+
   })
 
 } )
